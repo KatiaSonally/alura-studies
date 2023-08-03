d14fad047a2256e65e0ce2013c00e590635b7a8a
KatiaSonally <katiasonally@yahoo.com.br>
2023-08-03 11:58:40 -0300

    feat: implements application styling

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './style.scss';
 
 class Botao extends React.Component {
     render(): React.ReactNode {
         return (
-            <button>
+            <button className='botao'>
                 Botão
             </button>
         )
