f59634536e5999cf049a3c9b276a93fca049c65f
KatiaSonally <katiasonally@yahoo.com.br>
2023-08-06 16:39:37 -0300

    feat: implements the conversions to make the timer dynamic

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,8 +1,10 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss'
+import { tempoParaSegundos } from "../../commom/utils/date";
 
 export default function Cronometro() {
+    console.log('conversao', tempoParaSegundos('01:01:01'))
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
