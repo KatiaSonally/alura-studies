75a2e070be076e7fcc8d5394542e1e80306da5ec
KatiaSonally <katiasonally@yahoo.com.br>
2023-08-06 16:08:04 -0300

    feat: creates the selected state according to the click

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,14 +2,20 @@ import style from './Lista.module.scss';
 import Item from "./Item";
 import { ITarefa } from '../../types/tarefa';
 
-function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
     return (
         <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
             <ul>
-                {tarefas.map((item, index) => (
+                {tarefas.map(item => (
                     <Item
-                    key={index}
+                    selecionaTarefa={selecionaTarefa}
+                    key={item.id}
                         {...item}
                     />
                 ))}                
