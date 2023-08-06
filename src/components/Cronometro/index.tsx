b1806c09e4d8cb0a672c214cc19e7f8f3ff8e1a1
KatiaSonally <katiasonally@yahoo.com.br>
2023-08-06 18:00:22 -0300

    feat: passes to the timer the converted time value of the chosen task

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,15 +1,28 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss'
-import { tempoParaSegundos } from "../../commom/utils/date";
+import { tempoParaSegundos } from "../../commom/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 
-export default function Cronometro() {
-    console.log('conversao', tempoParaSegundos('01:01:01'))
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({ selecionado }: Props) {
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if(selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado]);
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo}/>
             </div>
             <button>
                 Começar!
